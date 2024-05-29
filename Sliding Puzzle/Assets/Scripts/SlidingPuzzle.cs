f49a79a7aaf29c98fa7a07592023814ff2ad32f3
Luke <luke.liu@aktsk.com>
2024-05-29 15:19:38 +0800

    Cell 的 Move Logic製作

--- Sliding Puzzle/Assets/Scripts/SlidingPuzzle.cs
+++ Sliding Puzzle/Assets/Scripts/SlidingPuzzle.cs
@@ -16,9 +16,6 @@ public class SlidingPuzzle : MonoBehaviour
     private GameObject[,] gameCells;
     private Vector2[,] initialPositions;
     private Vector2[,] currentPositions;
-    private GameObject draggingCell = null;
-    private Vector2 draggingCellStartPos;
-    private Vector2 mouseStartPos;
     private BoxCollider2D boxCollider;
 
     void Start()
@@ -75,10 +72,17 @@ public class SlidingPuzzle : MonoBehaviour
                 newCell.transform.localScale = new Vector3(cellWidth / cellSprite.bounds.size.x, cellHeight / cellSprite.bounds.size.y, 1);
 
                 // Position the cell
-                Vector2 cellPos = new Vector2(startX + i * (cellWidth + space), -1*(startY - j * (cellHeight + space)));
+                Vector2 cellPos = new Vector2(startX + i * (cellWidth + space), -1 * (startY - j * (cellHeight + space)));
                 newCell.transform.position = cellPos;
                 initialPositions[i, j] = cellPos;
                 currentPositions[i, j] = cellPos;
+
+                // 存儲初始位置到PlayMaker FSM變量
+                PlayMakerFSM cellFSM = newCell.GetComponent<PlayMakerFSM>();
+                if (cellFSM != null)
+                {
+                    cellFSM.FsmVariables.GetFsmVector2("InitialPosition").Value = cellPos;
+                }
             }
         }
     }
@@ -111,108 +115,6 @@ public class SlidingPuzzle : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            OnMouseDown();
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            OnMouseUp();
-        }
-
-        if (draggingCell != null)
-        {
-            OnMouseDrag();
-        }
-    }
-
-    void OnMouseDown()
-    {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Collider2D hit = Physics2D.OverlapPoint(mousePos);
-
-        if (hit != null && hit.transform.parent == transform)
-        {
-            draggingCell = hit.gameObject;
-            draggingCellStartPos = draggingCell.transform.position;
-            mouseStartPos = mousePos;
-        }
-    }
-
-    void OnMouseDrag()
-    {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 delta = mousePos - mouseStartPos;
-        draggingCell.transform.position = draggingCellStartPos + delta;
-
-        // Check for swap
-        for (int i = 0; i < M; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                if (gameCells[i, j] == draggingCell)
-                    continue;
-
-                Vector2 cellPos = currentPositions[i, j];
-                if (Vector2.Distance(draggingCell.transform.position, cellPos) < (cellPrefab.GetComponent<SpriteRenderer>().bounds.size.x / 2))
-                {
-                    SwapCells(draggingCell, gameCells[i, j]);
-                    return;
-                }
-            }
-        }
-    }
-
-    void OnMouseUp()
-    {
-        if (draggingCell != null)
-        {
-            draggingCell.transform.position = draggingCellStartPos;
-            draggingCell = null;
-
-            // Check for stage clear
-            if (IsStageClear())
-            {
-                fsm.SendEvent("StageClear");
-            }
-        }
-    }
-
-    void SwapCells(GameObject cellA, GameObject cellB)
-    {
-        Vector2 tempPos = cellA.transform.position;
-        cellA.transform.position = cellB.transform.position;
-        cellB.transform.position = tempPos;
-
-        // Update current positions
-        for (int i = 0; i < M; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                if (gameCells[i, j] == cellA)
-                {
-                    currentPositions[i, j] = cellA.transform.position;
-                }
-                else if (gameCells[i, j] == cellB)
-                {
-                    currentPositions[i, j] = cellB.transform.position;
-                }
-            }
-        }
-    }
-
-    bool IsStageClear()
-    {
-        for (int i = 0; i < M; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                if (currentPositions[i, j] != initialPositions[i, j])
-                {
-                    return false;
-                }
-            }
-        }
-        return true;
+        // 將拖曳和交換的邏輯移除，以便使用Playmaker實現
     }
 }
